e4a32687ae6b02a67f3f16224139e02f1d7c2c9b
lind1125 <danieljlinden@gmail.com>
2020-12-05 18:50:51 -0600

    initial organization

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+// React router
+import {BrowserRouter, Switch} from "react-router-dom"
 
+
+// Components imports
+import App from "./App";
+// CSS imports
+import "./css/index.css";
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* BrowserRouter -> Switch -> App */}
+    <BrowserRouter>
+      <Switch>
+        <App />
+      </Switch>
+    </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
